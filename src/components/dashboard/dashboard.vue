6a624454eda589665b6600324d718579c41311a0
Camillo <cangelozzi@gmail.com>
2018-06-17 19:24:28 -0400

    first commit

---
+++ src/components/dashboard/dashboard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="dashboard">
+    <h1>That's the dashboard!</h1>
+    <p>You should only get here if you're authenticated!</p>
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+
+  //! AXOS GET REQUEST
+  created() {
+    axios.get('https://vuejs-axios-project.firebaseio.com/users.json')
+      .then(res => console.log(res))
+      .catch(error => console.log(error))
+  }
+
+}
+</script>
+
+
+<style scoped>
+  h1, p {
+    text-align: center;
+  }
+
+  p {
+    color: red;
+  }
+</style>
